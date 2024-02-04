5be1cde82ab05744a5e62bd57b4abd1242fc32e6
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2024-02-04 16:27:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Task 1
+var myName = "Sura"; 
+var age = 24;      
+var isStudent = true; 
+
+console.log("Name:", myName);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// Task 2
+var variableToCheck = 24; 
+var dataType = typeof variableToCheck;
+console.log("Data type of variableToCheck:", dataType);
+
+// Task 3
+var greeting = "Hello";
+var theName = "Sura";
+console.log(greeting , theName);
+
+//Homework - intro-to-js-hw2
+// Task 2
+var a = 20;
+var b = 10;
+
+console.log("Is a greater than b?", a > b);    
+console.log("Is a less than b?", a < b);      
+console.log("Is a greater than or equal to b?", a >= b);  
+console.log("Is a less than or equal to b?", a <= b);     
+console.log("Is a equal to b?", a == b);      
+console.log("Is a not equal to b?", a != b); 
