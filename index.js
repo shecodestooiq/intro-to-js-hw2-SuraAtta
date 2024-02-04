ba6793271dded1c2a9933b6642add2b7bfc18ad1
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2024-02-04 23:39:07 +0300

    hw2

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-
-// Task 1
-var myName = "Sura"; 
-var age = 24;      
-var isStudent = true; 
-
-console.log("Name:", myName);
-console.log("Age:", age);
-console.log("Is Student:", isStudent);
-
-// Task 2
-var variableToCheck = 24; 
-var dataType = typeof variableToCheck;
-console.log("Data type of variableToCheck:", dataType);
-
-// Task 3
-var greeting = "Hello";
-var theName = "Sura";
-console.log(greeting +" "+ theName);
-
 //Homework - intro-to-js-hw2
 // Task 
 var a = 20;
